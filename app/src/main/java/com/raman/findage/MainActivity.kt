ad35458c130e5294d35079a9d493e98a71740f07
Raman Sidhu <raman.ss.sidhu@gmail.com>
2019-10-08 20:08:05 +0530

    project complete

--- app/src/main/java/com/raman/findage/MainActivity.kt
+++ app/src/main/java/com/raman/findage/MainActivity.kt
@@ -2,11 +2,19 @@ package com.raman.findage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        buGetAge.setOnClickListener {
+            val userDOB = Integer.parseInt(etDOB.text.toString())
+            val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+            val userAgeInYears = currentYear - userDOB
+            tvShowAge.text = "Your Age is $userAgeInYears Years"
+        }
     }
 }
